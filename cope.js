1a900e0a71692f8aac6cbb11da88d00d01ae2064
thiago-woro <nickf12@hotmail.com>
2023-11-02 11:04:07 -0300

    adicionado todos os links de categories do cooper

--- cope.js
+++ cope.js
@@ -5,7 +5,8 @@ const {autoScroll} = require("./autoscroll.js");
 
 let extractedProducts = []; // Global variable to store extracted products
 let baseurl = "https://www.minhacooper.com.br/loja/a.verde-bnu/produto/listar/";
-let categoriesURLs = [420];
+let categoriesURLs =  [1, 222, 525, 420, 75, 1033, 384, 281, 28, 647, 1145, 482, 464, 428, 503, 431, 421, 439, 458, 1134, 446, 451, 455, 1090, 5, 9, 12, 219, 18, 915, 1112, 40, 950, 60, 228, 126, 142, 165, 1100, 47, 197, 2, 96, 894, 236, 278, 63, 311, 321, 302, 323, 308, 1084, 363, 349, 319, 282, 351, 313, 370, 379, 1076]
+
 
 async function setupBrowser() {
 	const browser = await puppeteer.launch({headless: false, slowMo: 0, devtools: false});
@@ -26,10 +27,18 @@ async function scrapeCategory(categoryURL, page) {
 		console.log(`\n\n⏳ loading: ${categoryURL}`);
 
 		// Navigate to the category URL
-		await page.goto(baseurl + categoryURL);
-		await page.waitForTimeout(900); // Wait for 4.3 seconds (4300 milliseconds)
+		await page.goto(baseurl + categoryURL, {waitUntil: "domcontentloaded", timeout: 0});
+		//await page.waitForTimeout(900); // Wait for 4.3 seconds (4300 milliseconds)
+		console.log("loaded dom");
 
-		//await autoScroll(page);
+		await autoScroll(page);
+
+		/* 
+		await page.waitForNavigation({
+		  waitUntil: 'networkidle0',
+		});
+		console.log('network idle');
+	   */
 
 		let loadMoreButtonExists = true;
 		while (loadMoreButtonExists) {
@@ -64,21 +73,32 @@ async function scrapeCategory(categoryURL, page) {
 				// Extract product name
 				product.name = await productContainer.$eval(".product-variation__name", (a) => a.textContent.trim());
 
+				// Extract fractional description element within the current product container
+				const fractionalDescElement = await productContainer.$(".product-variation__fractional-desc");
+
+				if (fractionalDescElement) {
+					// Extract the price text within the "span" element
+					const priceKg = await fractionalDescElement.$eval("span", (span) => span.textContent.trim());
+					product.pricePorQuilo = priceKg;
+					console.log(priceKg);
+				}
+
+				// Extract the final price within the current product container
+				const finalPriceElement = await productContainer.$(".product-variation__final-price");
+
+				if (finalPriceElement) {
+					// Extract the price text within the "span" element
+					product.price = await finalPriceElement.evaluate(node => node.textContent.trim());
+					console.log(product.price);
+				}
+
 				// Extract the image URL
 				product.imageUrl = await productContainer.$eval("img.product-variation__image", (img) => img.getAttribute("src"));
 
 				// Extract product link
 				product.productLink = await productContainer.$eval("a.product-variation__image-container", (a) => a.getAttribute("href"));
 
-				// Extract prices
-				const priceElement = await productContainer.$(".product-variation__price");
-
-				// Extract product price
-				product.price = await productContainer.$eval(".product-variation__final-price", (span) => span.textContent.trim());
-
-				//product.priceCooper = await productContainer.$eval(".product-variation__cooper-price", (span) => span.textContent.trim());
-
-
+				/* 
 				// Check if cooperPrice exists
 				const cooperPriceElement = await priceElement.$(".product-variation__cooper-price");
 				if (cooperPriceElement) {
@@ -87,9 +107,7 @@ async function scrapeCategory(categoryURL, page) {
 				} else {
 					product.cooperPrice = null; // Set to null if cooperPrice is not found
 				}
-
-
-
+ */
 
 				// Add the product to the array
 				products.push(product);
@@ -141,7 +159,6 @@ function addProductDetails(products) {
 	//await removeDuplicates();  //fix product names
 
 	// Log the accumulated extracted product information after removing duplicates
-	console.log(extractedProducts);
 	console.log("\nTotal products after removing duplicates: ", extractedProducts.length);
 
 	// Close the browser after all categories have been scraped
